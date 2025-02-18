b17df959c7a621a35d414980a2b00dd67e444de6
tuantran47 <21110810@student.hcmute.edu.vn>
2025-02-18 23:09:25 +0700

    board bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -15,15 +15,17 @@ import Badge from '@mui/material/Badge'
 import Tooltip from '@mui/material/Tooltip'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import Profiles from './Menus/Profiles'
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
 
 function AppBar() {
   return (
-    <Box px={ 2 } sx={ {
+    <Box sx={ {
       width: '100%',
       height: ( them ) => them.trelloCustom.appBarHeight,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
+      paddingX: 2,
       gap: 2,
       overflowX: 'auto'
     } }>
@@ -38,7 +40,7 @@ function AppBar() {
           <Recent />
           <Starred />
           <Templates />
-          <Button variant="outlined">Create</Button>
+          <Button variant="outlined" startIcon={ <LibraryAddIcon /> }>Create</Button>
         </Box>
       </Box>
       <Box sx={ { display: 'flex', alignItems: 'center', gap: 2 } }>
