2b42b45f4102fcaaf3192999cbd210a1d44cb5b2
tuantran47 <21110810@student.hcmute.edu.vn>
2025-02-28 14:14:15 +0700

    protected route

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { activeBoardReducer } from './activeBoard/activeBoardSlice'
 import { userReduce } from '~/redux/user/userSlice'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // default la local storage
+
+// Config persist
+const rootPersistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['user'] // định nghĩa các slice ĐƯỢC PHÉP duy trì qua mỗi lần F5 trình duyệt
+  // blacklist: ['user'] // định nghĩa các slice KHÔNG ĐƯỢC PHÉP duy trì qua mỗi lần F5 trình duyệt
+}
+
+// Combine các reduces trong dự án của chúng ta ở đây
+const reduces = combineReducers({
+  activeBoard: activeBoardReducer,
+  user: userReduce
+})
+
+// Thực hiện persist reducer
+const persistedReducer = persistReducer(rootPersistConfig, reduces)
+
 export const store = configureStore({
-  reducer: {
-    activeBoard: activeBoardReducer,
-    user: userReduce
-  }
+  reducer: persistedReducer,
+  // Fix warning error when implement redux-persist
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
 })
