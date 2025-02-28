0dbcfb059cc9ec7312d3152a51261a23ec044a5e
tuantran47 <21110810@student.hcmute.edu.vn>
2025-02-28 19:57:15 +0700

    refresh token

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
 import authorizedAxiosInstance from '~/utils/authorizeAxios'
 import { API_ROOT } from '~/utils/constants'
 
@@ -13,6 +14,18 @@ export const loginUserAPI = createAsyncThunk(
     return response.data
   }
 )
+
+export const logoutUserAPI = createAsyncThunk(
+  'user/logoutUserAPI',
+  async (showSuccessMessage = true ) => {
+    const response = await authorizedAxiosInstance.delete(`${API_ROOT}/v1/users/logout`)
+    if ( showSuccessMessage ) {
+      toast.success('Logged out successfully!')
+    }
+    return response.data
+  }
+)
+
 export const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -21,6 +34,13 @@ export const userSlice = createSlice({
     builder.addCase(loginUserAPI.fulfilled, (state, action) => {
       const user = action.payload
       state.currentUser = user
+    })
+    builder.addCase(logoutUserAPI.fulfilled, (state) => {
+      /**
+       * API logout sau khi gọi thành công thì sẽ clear thông tin currentUser về null ở đây
+       * Kết hợp ProtectedRoute đã làm ở App.js => code điều hướng về trang login
+       */
+      state.currentUser = null
     })
   }
 })
--- src/utils/authorizeAxios.js
+++ src/utils/authorizeAxios.js
@@ -1,6 +1,14 @@
+/* eslint-disable no-unused-vars */
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { interceptorLoadingElements } from '~/utils/formatters'
+import { refreshTokenAPI} from '~/apis'
+import { logoutUserAPI } from '~/redux/user/userSlice'
+
+
+let axiosReduxStore
+export const injectStore = mainStore => {axiosReduxStore = mainStore}
+
 
 // Khởi tạo một đối tượng Axios ( authorizedAxiosInstance) mục đích để custom và cấu hình chung cho dự án.
 let authorizedAxiosInstance = axios.create()
@@ -23,6 +31,11 @@ authorizedAxiosInstance.interceptors.request.use( (config) => {
   return Promise.reject(error)
 })
 
+// Khởi tạo một cái promise cho việc gọi api refresh_token
+// Mục đích tạo Promise này để khi nào gọi api refresh_token xong xuôi thì mới retry lại nhiều api bị lỗi trước đó
+
+let refreshTokenPromise = null
+
 // Interceptor Response: Can thiệp vào giữa những response API.
 authorizedAxiosInstance.interceptors.response.use( (response) => {
   // Kỹ thuật chặn spam click ( Xem kỹ trong file formatters chứa function)
@@ -37,6 +50,38 @@ authorizedAxiosInstance.interceptors.response.use( (response) => {
   // Kỹ thuật chặn spam click ( Xem kỹ trong file formatters chứa function)
   interceptorLoadingElements(false)
 
+  /* Xử Lý Refresh Token tự động */
+  // Trường hợp 1: Nếu như nhận mã 401 từ BE gọi gọi API đăng xuất luôn
+  if ( error.response?.status === 401) {
+    axiosReduxStore.dispatch( logoutUserAPI(false) )
+  }
+
+  // Trường hợp 2: Nếu nhận mã 410 từ BE, thì sẽ gọi refresh token để làm mới lại refresh token
+  // Đầu tiên lấy được các request API đang bị lỗi thông qua error.config
+  const originalRequests = error.config
+  if ( error.response?.status === 410 && originalRequests ) {
+    if (!refreshTokenPromise) {
+      // Gọi API refToken
+      refreshTokenPromise = refreshTokenAPI()
+        .then(data => {
+          return data?.accessToken
+        })
+        .catch((err) => {
+          // Nhận bất kỳ lỗi nào ở API refresh Token thì đăng xuất luôn.
+          axiosReduxStore.dispatch( logoutUserAPI(false) )
+          return Promise.reject(err)
+        }).finally(() => {
+          refreshTokenPromise = null
+        })
+    }
+
+    return refreshTokenPromise.then( accessToken => {
+      // Đối với các trường hợp cần lưu lại accessToken hoặc refreshToken vào localStorage thì sẽ thêm code xử lý ở đây
+
+      // return axios instant để gọi lại các api bị lỗi ban đầu
+      return authorizedAxiosInstance(originalRequests)
+    })
+  }
   // Xử lý tập trung phần hiển thị thông báo lỗi và trả về từ mọi API ở đây (viết code 1 lần clean code)
   // console.log error ra là sẽ thấy cấu trúc data dẫn tới message lỗi như dưới đây
   let errorMessage = error?.message
